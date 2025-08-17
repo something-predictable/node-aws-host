2942bd642b60f8ba38f9734125187adb6be00ac8
k15r <git@riddance.k15r.xyz>
2025-08-17 22:23:39 +0200

    Timer support.

---
+++ timer.ts
@@ -0,0 +1,65 @@
+import { measure } from '@riddance/host/lib/timer'
+import { getHandlers } from '@riddance/host/registry'
+import { triggerTimer } from '@riddance/host/timer'
+import { AwsContext, createAwsContext } from './context.js'
+
+export * from '@riddance/service/timer'
+
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/b969f890000ff95740fd7b879cdf3b73e1ea0fe8/types/aws-lambda/trigger/eventbridge.d.ts
+
+type EventBridgeEvent = {
+    version: '0'
+    id: string
+    account: string
+    time: string
+    region: string
+    resources: string[]
+    source: string
+}
+
+async function asyncIndex(
+    event: EventBridgeEvent,
+    awsContext: AwsContext,
+    callback: (error: unknown) => void,
+) {
+    const [handler] = getHandlers('timer')
+    if (!handler) {
+        throw new Error('No timer handler registered.')
+    }
+    const { log, context, success, flush } = createAwsContext(
+        awsContext,
+        {},
+        {
+            operationId: event.id,
+        },
+        handler.config,
+        handler.meta,
+    )
+
+    await triggerTimer(
+        log,
+        context,
+        handler,
+        {
+            triggerTime: new Date(event.time),
+        },
+        success,
+    )
+
+    try {
+        callback(undefined)
+    } catch (e) {
+        log.fatal('Error sending result to Lambda.', e)
+    }
+
+    await measure(log, 'flush', flush)
+}
+
+export function awsHandler(
+    event: EventBridgeEvent,
+    context: AwsContext,
+    callback: (error: unknown) => void,
+) {
+    context.callbackWaitsForEmptyEventLoop = false
+    asyncIndex(event, context, callback).catch((e: unknown) => setImmediate(callback, e))
+}
